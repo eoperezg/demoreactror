eaf623f74d0cb1037846684b9f0ca7209f3a6467
Edison Pérez González <edison.p_93@hotmail.cl>
2023-03-09 23:19:50 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,5 @@
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = 3.26156 * parsecs
+
+print(parsecs_input + " parasecs is " + str(lightyears) + " lightyears")
